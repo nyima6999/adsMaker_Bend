bf51f97e9282a88ebd4553a9fa1543e91d3950c9
nyima6999 <nyima6999@gmail.com>
2022-04-21 16:22:29 -0700

    user registration API created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,37 @@
-const registerUser = async (req, res) => {
+const User = require("../models/UserModel");
+const asyncHandler = require("express-async-handler");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
+  // define user data
+  const userExists = await User.find({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("user already exist");
+    // if user exist in mongoose , throw this error
+  }
+  // if user not exist, create a new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  //  if user successfully created, response with status 201
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured");
+  }
+
   res.json({
     name,
     email,
   });
-};
+});
 module.exports = { registerUser };
---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, minlength: 4 },
+    email: { type: String, requred: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// ecrypting user password/ pre-before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// matching password entered with password in database
+userSchema.methods.matchPassword = async function (passwordEntered) {
+  return await bcrypt.compare(passwordEntered, this.password);
+  // this.password; the password coming from the database
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
